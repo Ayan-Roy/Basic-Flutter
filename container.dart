0e9b40425db4093670d7dad2b16693be6c5dfe7d
Ayan Roy <51925552+Ayan-Roy@users.noreply.github.com>
2022-10-28 18:32:23 +0600

    Create container.dart

---
+++ container.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black45,
+      appBar: AppBar(
+        title: Text('Home'.toLowerCase()),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+        height: 200.0,
+        width: 300.0,
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.fromLTRB(10, 20, 10, 10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.tealAccent,
+          //borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0)),
+          shape: BoxShape.circle,
+          image: DecorationImage(image: AssetImage("images/InShot_20211121_153219112.jpg")),
+
+        ),
+        child: Text(
+          "Ayan Roy",
+          style: TextStyle(
+            fontSize: 24.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+          
+        ),
+      ),
+    );
+  }
+}
+
+int getRandom() {
+  return Random().nextInt(100);
+}
